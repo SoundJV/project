4228ebcf4a9da39c39ab8253ad41799122d93dbe
SoundJV <soundjv2020@gmail.com>
2020-09-21 16:43:22 +0200

    youtubeInt

--- src/App.js
+++ src/App.js
@@ -23,7 +23,8 @@ function App() {
       <div className="test">
         <iframe src="https://open.spotify.com/embed/playlist/37i9dQZF1DX7qQG2hCGiwy" width="300" height="380" frameborder="0" allowtransparency="true" allow="encrypted-media"></iframe>
         <iframe scrolling="no" frameborder="0" allowTransparency="true" src="https://www.deezer.com/plugins/player?format=square&autoplay=false&playlist=false&width=300&height=300&color=007FEB&layout=dark&size=medium&type=playlist&id=30595446&app_id=1" width="300" height="300"></iframe>
-      </div>
+        <iframe width="1280" height="720" src="https://www.youtube.com/embed/8tpwt3nB7nM" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+</div>
     </div>
 
   );
