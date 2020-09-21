a231aea155e93559e9aa14b6cc161e828d592dd5
Walid <walidpsg78@outlook.fr>
2020-09-21 16:33:38 +0200

    cotacote

--- src/App.js
+++ src/App.js
@@ -22,11 +22,8 @@ function App() {
       </header>
       <div className="test">
         <iframe src="https://open.spotify.com/embed/playlist/37i9dQZF1DX7qQG2hCGiwy" width="300" height="380" frameborder="0" allowtransparency="true" allow="encrypted-media"></iframe>
+        <iframe scrolling="no" frameborder="0" allowTransparency="true" src="https://www.deezer.com/plugins/player?format=square&autoplay=false&playlist=false&width=300&height=300&color=007FEB&layout=dark&size=medium&type=playlist&id=30595446&app_id=1" width="300" height="300"></iframe>
       </div>
-      <div>
-      <iframe scrolling="no" frameborder="0" allowTransparency="true" src="https://www.deezer.com/plugins/player?format=square&autoplay=false&playlist=false&width=300&height=300&color=007FEB&layout=dark&size=medium&type=playlist&id=30595446&app_id=1" width="300" height="300"></iframe>
-      </div>
-
     </div>
 
   );
