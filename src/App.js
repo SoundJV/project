5be58e3d95a149f65bfa69970edbdf3552ec03c1
Walid <walidpsg78@outlook.fr>
2020-09-22 22:27:51 +0200

    Route

--- src/App.js
+++ src/App.js
@@ -1,32 +1,20 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Suspense } from 'react';
 import './App.css';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+
+const Home = React.lazy(() => {
+  return import('./screens/Home');
+});
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React 1
-          Learn react 2h
-        </a>
-      </header>
-      <div className="test">
-        <iframe src="https://open.spotify.com/embed/playlist/37i9dQZF1DX7qQG2hCGiwy" width="300" height="380" frameborder="0" allowtransparency="true" allow="encrypted-media"></iframe>
-        <iframe scrolling="no" frameborder="0" allowTransparency="true" src="https://www.deezer.com/plugins/player?format=square&autoplay=false&playlist=false&width=300&height=300&color=007FEB&layout=dark&size=medium&type=playlist&id=30595446&app_id=1" width="300" height="300"></iframe>
-        <iframe width="1280" height="720" src="https://www.youtube.com/embed/8tpwt3nB7nM" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-      </div>
-    </div>
-
+    <Router>
+      <Suspense fallback={<div>Chargement...</div>}>
+        <Switch>
+          <Route exact path="/" component={Home} />
+        </Switch>
+      </Suspense>
+    </Router>
   );
 }
 
---
+++ src/screens/Home.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import logo from '../assets/logo.svg';
+import './Home.css';
+
+
+function Home() {
+    return (
+        <div className="App">
+            <header className="App-header">
+                <img src={logo} className="App-logo" alt="logo" />
+                <p>
+                    Edit <code>src/App.js</code> and save to reload.
+        </p>
+                <a
+                    className="App-link"
+                    href="https://reactjs.org"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                >
+                    Learn React 1
+                    Learn react 2h
+        </a>
+            </header>
+            <div className="test">
+                <iframe src="https://open.spotify.com/embed/playlist/37i9dQZF1DX7qQG2hCGiwy" width="300" height="380" frameborder="0" allowtransparency="true" allow="encrypted-media"></iframe>
+                <iframe scrolling="no" frameborder="0" allowTransparency="true" src="https://www.deezer.com/plugins/player?format=square&autoplay=false&playlist=false&width=300&height=300&color=007FEB&layout=dark&size=medium&type=playlist&id=30595446&app_id=1" width="300" height="300"></iframe>
+                <iframe width="1280" height="720" src="https://www.youtube.com/embed/8tpwt3nB7nM" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+            </div>
+        </div>
+
+    );
+}
+
+export default Home;
